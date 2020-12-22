4789889bc62a655b5fb35964c3a50c744964ddba
JB <joshbrown2794@gmail.com>
2020-12-21 21:12:11 -0800

    chart rendering

---
+++ src/LineGraph.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+function LineGraph() {
+  const data = [
+    {
+      x: 10,
+      y: 20,
+    },
+    { x: 15, y: 10 },
+  ];
+  return (
+    <div className="linegraph">
+      <Line
+        data={{
+          datasets: [
+            {
+              type: "line",
+              data: data,
+              backgroundColor: "black",
+              borderColor: "#5AC53B",
+              borderWidth: 2,
+              pointBorderColor: "rgba(0, 0, 0, 0)",
+              pointBackgroundColor: "rgba(0, 0, 0, 0)",
+              pointHoverBackgroundColor: "#5AC53B",
+              pointHoverBorderColor: "#000000",
+              pointHoverBorderWidth: 4,
+              pointHoverRadius: 6,
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+}
+
+export default LineGraph;
--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Newsfeed.css";
+import LineGraph from "./LineGraph.js";
 function Newsfeed() {
   return (
     <div className="newsfeed">
-      <div className="newsfeed_container"></div>
+      <div className="newsfeed_container">
+        <div className="newsfeed_chartSection">
+          <div className="newsfeed_portfolioValue">
+            <h1>$140,000</h1>
+            <p>$142,000</p>
+          </div>
+          <div className="newsfeed_chart">
+            <LineGraph />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
