0d9ef7a4d17d70fc517d4ba2d83c6b2cf8f4a42b
JB <joshbrown2794@gmail.com>
2020-12-28 23:59:30 -0800

    axios function

--- src/Stats.js
+++ src/Stats.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Stats.css";
+import axios from "axios";
+const TOKEN = "bvldmdf48v6qdeqd6470";
+
+const BASE_URL = "https://finnhub.io/api/v1/quote";
 function Stats() {
+  const [stockSata, setstockSata] = useState([]);
+  const getStockData = (stock) => {
+    return axios
+      .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+  useEffect(() => {}, []);
   return (
     <div className="stats">
       <div className="stats_container">
