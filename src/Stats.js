c2719c720990567ee94435f7dbcf09dd36429ced
JB <joshbrown2794@gmail.com>
2020-12-29 00:12:34 -0800

    sata to data

--- src/Stats.js
+++ src/Stats.js
@@ -5,7 +5,7 @@ const TOKEN = "bvldmdf48v6qdeqd6470";
 
 const BASE_URL = "https://finnhub.io/api/v1/quote";
 function Stats() {
-  const [stockSata, setstockSata] = useState([]);
+  const [stockData, setstockData] = useState([]);
   const getStocksData = (stock) => {
     return axios
       .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
