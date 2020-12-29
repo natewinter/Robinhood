fd00c2da81a58fd69327992594a0ca4440d5609a
JB <joshbrown2794@gmail.com>
2020-12-29 00:11:35 -0800

    temp data

--- src/Stats.js
+++ src/Stats.js
@@ -6,14 +6,38 @@ const TOKEN = "bvldmdf48v6qdeqd6470";
 const BASE_URL = "https://finnhub.io/api/v1/quote";
 function Stats() {
   const [stockSata, setstockSata] = useState([]);
-  const getStockData = (stock) => {
+  const getStocksData = (stock) => {
     return axios
       .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
       .catch((error) => {
         console.error("Error", error.message);
       });
   };
-  useEffect(() => {}, []);
+  useEffect(() => {
+    let tempStocksData = [];
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock).then((res) => {
+          console.log(res);
+          tempStocksData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+  }, []);
   return (
     <div className="stats">
       <div className="stats_container">
