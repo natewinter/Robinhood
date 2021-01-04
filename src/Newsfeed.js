f4e7d50e44150eae4c3ca53dff1eb590056c3ac6
natewinter <joshbrown2794@gmail.com>
2021-01-03 18:39:22 -0800

    saving time

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Newsfeed.css";
 import LineGraph from "./LineGraph.js";
+import TimeLine from "./TimeLine.js";
 function Newsfeed() {
   return (
     <div className="newsfeed">
@@ -12,6 +13,17 @@ function Newsfeed() {
           </div>
           <div className="newsfeed_chart">
             <LineGraph />
+            <TimeLine />
+          </div>
+        </div>
+        <div className="newsfeed_buying_section">
+          <h2>Buying Power</h2>
+          <h2>$4.11</h2>
+        </div>
+        <div className="newsfeed_market_section">
+          <div className="newsfeed_market_box">
+            <p>Markets Closed</p>
+            <h1>Happy Thanksgiving!</h1>
           </div>
         </div>
       </div>
